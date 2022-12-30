44c77d4ee2c63d528e49e900f971fc53f1519ede
manaswimancha <manaswim@buffalo.edu>
2022-12-30 09:35:13 -0500

    Finished common_word.py

---
+++ common_words.py
@@ -0,0 +1,5 @@
+with open("txt_files/pg69662.txt",encoding="utf8") as f:
+    print(f.read().count("the "))
+
+with open("txt_files/pg69659.txt",encoding="utf8") as f:
+    print(f.read().count("the "))
