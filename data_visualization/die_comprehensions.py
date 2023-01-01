5e4208cba37afef27865cfa69b22a31d3ce30dfd
manaswimancha <manaswim@buffalo.edu>
2023-01-01 10:43:59 -0500

    another commit

--- data_visualization/die_comprehensions.py
+++ data_visualization/die_comprehensions.py
@@ -1,47 +1,47 @@
-# # List comprehension for 2_d8s.py
+# List comprehension for 2_d8s.py
 
-# from plotly.graph_objs import Bar, Layout
-# from plotly import offline
-# from die import Die
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+from die import Die
 
-# die_1 = Die(8)
-# die_2 = Die(8)
+die_1 = Die(8)
+die_2 = Die(8)
 
-# results = [die_1.roll() + die_2.roll() for roll_num in range(1000)]
+results = [die_1.roll() + die_2.roll() for roll_num in range(1000)]
 
-# frequencies = [results.count(value) for value in range(2,die_1.num_sides + die_2.num_sides + 1)]
+frequencies = [results.count(value) for value in range(2,die_1.num_sides + die_2.num_sides + 1)]
 
-# x_values = [i for i in range(2,die_1.num_sides + die_2.num_sides + 1)]
+x_values = [i for i in range(2,die_1.num_sides + die_2.num_sides + 1)]
 
-# data = [Bar(x=x_values,y=frequencies)]
+data = [Bar(x=x_values,y=frequencies)]
 
-# x_axis_config = {"title":"Result","dtick":1}
-# y_axis_config = {"title":"Frequency of Result"}
-# my_layout = Layout(title="Results of rolling 2 D8 dice 1,000 times",xaxis=x_axis_config,yaxis=y_axis_config)
-# offline.plot({"data":data,"layout":my_layout},filename="2_d8s.html")
+x_axis_config = {"title":"Result","dtick":1}
+y_axis_config = {"title":"Frequency of Result"}
+my_layout = Layout(title="Results of rolling 2 D8 dice 1,000 times",xaxis=x_axis_config,yaxis=y_axis_config)
+offline.plot({"data":data,"layout":my_layout},filename="2_d8s.html")
 
-# # List comprehension for 3_dice.py
+# List comprehension for 3_dice.py
 
-# from plotly.graph_objs import Bar, Layout
-# from plotly import offline
-# from die import Die
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+from die import Die
 
-# die_1 = Die()
-# die_2 = Die()
-# die_3 = Die()
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
 
-# results = [die_1.roll() + die_2.roll() + die_3.roll() for roll_num in range(1000)]
+results = [die_1.roll() + die_2.roll() + die_3.roll() for roll_num in range(1000)]
 
-# frequencies = [results.count(value) for value in range(2,die_1.num_sides + die_2.num_sides + die_3.num_sides + 1)]
+frequencies = [results.count(value) for value in range(2,die_1.num_sides + die_2.num_sides + die_3.num_sides + 1)]
 
-# x_values = [i for i in range(2,die_1.num_sides + die_2.num_sides + die_3.num_sides + 1)]
+x_values = [i for i in range(2,die_1.num_sides + die_2.num_sides + die_3.num_sides + 1)]
 
-# data = [Bar(x=x_values,y=frequencies)]
+data = [Bar(x=x_values,y=frequencies)]
 
-# x_axis_config = {"title":"Result","dtick":1}
-# y_axis_config = {"title":"Frequency of Result"}
-# my_layout = Layout(title="Results of rolling 3 D6 dice 1,000 times",xaxis=x_axis_config,yaxis=y_axis_config)
-# offline.plot({"data":data,"layout":my_layout},filename="3_dice.html")
+x_axis_config = {"title":"Result","dtick":1}
+y_axis_config = {"title":"Frequency of Result"}
+my_layout = Layout(title="Results of rolling 3 D6 dice 1,000 times",xaxis=x_axis_config,yaxis=y_axis_config)
+offline.plot({"data":data,"layout":my_layout},filename="3_dice.html")
 
 # List comprehension for multiplication.py
 
---
+++ data_visualization/practicing_with_both_libraries.py
@@ -0,0 +1,29 @@
+# # RandomWalk on Plotly
+
+# import plotly.express as px
+# from random_walk import RandomWalk
+
+# rw = RandomWalk(1000)
+# rw.fill_walk()
+
+# fig = px.scatter(x=rw.x_values,y=rw.y_values)
+# fig.update_xaxes(showgrid=False,zeroline=False,visible=False)
+# fig.update_yaxes(showgrid=False,zeroline=False,visible=False)
+# fig.show()
+
+# Die Roll on Matplotlib
+
+import matplotlib.pyplot as plt
+from die import Die
+
+die = Die()
+
+results = [die.roll() for i in range(1000)]
+frequencies = [results.count(i) for i in range(1,7)]
+x_values = [i for i in range(1,7)]
+
+plt.bar(x_values,frequencies)
+plt.title("Die Roll")
+plt.xlabel("Number")
+plt.ylabel("Frequency")
+plt.show()
