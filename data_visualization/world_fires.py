b7e8c837e15d5718c48503502ad5efc37aa49930
manaswimancha <manaswim@buffalo.edu>
2023-01-04 12:55:04 -0500

    another commit

--- data_visualization/world_fires.py
+++ data_visualization/world_fires.py
@@ -1,3 +1,5 @@
+# Old Data
+
 import csv
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
@@ -10,7 +12,7 @@ with open(filename) as f:
     col = csv.reader(f)
     header = next(col)
     for row in col:
-        mags.append(float(row[2]))
+        mags.append(int(float(row[2])))
         lons.append(float(row[1]))
         lats.append(float(row[0]))
 
@@ -19,8 +21,43 @@ data = [{
     "lon": lons,
     "lat": lats,
     "marker": {
-        "size": [5*mag for mag in mags],
-        "color": [int(mag) for mag in mags],
+        "size": [mag/10 for mag in mags],
+        "color": mags,
+        "colorscale": "Viridis",
+        "reversescale": True,
+        "colorbar": {"title": "Brightness"}
+    }
+}]
+
+my_layout = Layout(title="World Fires")
+fig = {"data":data,"layout":my_layout}
+offline.plot(fig,filename="world_fires.html")
+
+# New Data
+
+import csv
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+filename = "data_visualization/data/wf.csv"
+
+mags, lons, lats = [], [], []
+
+with open(filename) as f:
+    col = csv.reader(f)
+    header = next(col)
+    for row in col:
+        mags.append(int(float(row[2])))
+        lons.append(float(row[1]))
+        lats.append(float(row[0]))
+
+data = [{
+    "type": "scattergeo",
+    "lon": lons,
+    "lat": lats,
+    "marker": {
+        "size": [mag/10 for mag in mags],
+        "color": mags,
         "colorscale": "Viridis",
         "reversescale": True,
         "colorbar": {"title": "Brightness"}
